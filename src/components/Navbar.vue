20ae9ab2f9b2fbb1a0ca590e03b8354d7f70bb6d
Oluwatimileyin Akinpelu <72369149+Olaoluwadunni@users.noreply.github.com>
2021-11-25 00:37:12 +0100

    login screen and fixes for the landing page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,19 @@
 <template>
-    <nav class='container navbar-expand-lg flex-row'>
-        <div class='d-flex justify-content-between mt-4 pt-1'>
+    <nav class='nav-bar navbar navbar-expand-lg flex-row'>
+        <div class=' container mt-4 pt-1'>
             <div class=''>
                 <img src="../assets/images/enyataLogo.png" class="img-fluid pt-4" alt="enyataLogo">
-                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
             </div>
-            <div class='navbar  mb-2 mb-lg-0'>
-                <router-link to="/" class="nav-link">Home</router-link> 
-                <router-link to="/sign-in" class="nav-link">Sign in</router-link>
-                <router-link to="/register" class="nav-link" role="button" ><Button btnStyle='btn btn-lg btn-register-white' btnText='Register Now'></Button></router-link>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="nav-links navbar-nav mb-2 mb-lg-0">
+                    <router-link to="/" class="nav-link">Home</router-link> 
+                    <router-link to="/signin" class="nav-link">Sign in</router-link>
+                    <router-link to="/register" class="nav-link" role="button" ><Button btnStyle='btn btn-lg btn-register-white' btnText='Register Now'></Button></router-link>
+                </div>
             </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars fs-4 navbar-toggler-icon"></i>
+            </button>
         </div>
     </nav>
 </template>
