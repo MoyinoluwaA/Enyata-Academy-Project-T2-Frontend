b71b11b2f7d78d6d5857977a5d74b97ed985d5db
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-11-25 02:03:44 +0100

    updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,30 +1,25 @@
 <template>
-    <nav class='nav-bar navbar navbar-expand-lg flex-row'>
-        <div class=' container mt-4 pt-1'>
-            <div class=''>
-                <img src="../assets/images/enyataLogo.png" class="img-fluid pt-4" alt="enyataLogo">
-            </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="nav-links navbar-nav mb-2 mb-lg-0">
-                    <router-link to="/" class="nav-link">Home</router-link> 
-                    <router-link to="/signin" class="nav-link">Sign in</router-link>
-                    <router-link to="/signup" class="nav-link" role="button" ><Button btnStyle='btn btn-lg btn-register-white' btnText='Register Now'></Button></router-link>
-                </div>
-            </div>
+    <nav class='nav-bar navbar navbar-expand-md flex-row'>
+        <div class='container mt-4 pt-2 px-xl-5'>
+            <img src="../assets/images/enyataLogo.png" class="img-fluid pt-4" alt="enyataLogo">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-bars fs-4 navbar-toggler-icon"></i>
             </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="nav-links navbar-nav align-items-md-center mb-2 mb-lg-0 ms-md-auto">
+                    <router-link to="/" class="nav-link me-4 pe-3">Home</router-link> 
+                    <router-link to="/signin" class="nav-link me-4 pe-3">Sign in</router-link>
+                    <router-link to="/signup" class="nav-link btn btn-lg btn-register-white" role="button">
+                        Register Now
+                    </router-link>
+                </div>
+            </div>
         </div>
     </nav>
 </template>
 
 <script>
-import Button from '../components/Button.vue'
-
 export default {
-    name: 'Navbar',
-    components: {
-        Button
-    }
+    name: 'Navbar'
 }
 </script> 
