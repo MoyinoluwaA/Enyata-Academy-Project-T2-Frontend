d97e33fcfc1bf78a27d3c37c5ed0edb6dc4868f4
Oluwatimileyin Akinpelu <72369149+Olaoluwadunni@users.noreply.github.com>
2021-11-25 01:27:40 +0100

    implemented login screen, made changes to landing page and some routings

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
                 <div class="nav-links navbar-nav mb-2 mb-lg-0">
                     <router-link to="/" class="nav-link">Home</router-link> 
                     <router-link to="/signin" class="nav-link">Sign in</router-link>
-                    <router-link to="/register" class="nav-link" role="button" ><Button btnStyle='btn btn-lg btn-register-white' btnText='Register Now'></Button></router-link>
+                    <router-link to="/signup" class="nav-link" role="button" ><Button btnStyle='btn btn-lg btn-register-white' btnText='Register Now'></Button></router-link>
                 </div>
             </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -25,7 +25,7 @@
                     <div class="d-flex flex-column justify-content-center align-items-center">
                         <button class="btn col-md-6 col-12 btn-login mt-3" id="submit-btn" type="submit">Sign In</button>
                         <div class="d-flex col-md-6 col-12 justify-content-between fst-italic mt-2">
-                            <p class='labelForm text--labelColor'> Don't have an account yet? Sign Up</p>
+                            <p class='labelForm text--labelColor'> Don't have an account yet? <router-link to="/signup">Sign Up</router-link></p>
                             <p class='labelForm text--labelColor'>Forgot Password?</p>
                         </div>
                     </div>
