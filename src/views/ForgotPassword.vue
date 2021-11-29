9de10416e7637e6633b2f5eb9f38f305435f0675
Temitope3665 <aroyewontope@gmail.com>
2021-11-29 20:29:19 +0100

    cursor-pointer in reset password via email

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="container password d-flex align-items-center">
-        <div class="reset-via-email row flex-md-column align-items-center justify-content-center password-box mx-auto">
+        <div class="row flex-md-column align-items-center justify-content-center password-box mx-auto">
             <div class="text-center">
                 <h3 class="fw-bold password-heading text--primary mb-3">Forgot Password</h3>
                 <p class="text--gray-dark">Please select option to send link to reset password</p>
@@ -9,7 +9,7 @@
                     <div class="d-flex align-items-center me-3 me-sm-4">
                         <img src="../assets/icons/Group 12.svg" alt="email">
                     </div>
-                    <div>
+                    <div class="reset-via-email">
                         <h6 class="password-reset-title text--purple">Reset via Email</h6>
                         <p class="password-reset-text text-small mt-3 me-2 me-sm-0 text--gray">
                             Reset password link will be send to your
