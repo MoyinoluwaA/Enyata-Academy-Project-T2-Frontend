6923f04ef3f920e8861d74b6c15c7c70b9ca6b74
Temitope3665 <aroyewontope@gmail.com>
2021-11-30 18:12:27 +0100

    sign in validated, admin dashboard updated

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -8,20 +8,25 @@
             <form class="action">
                 <formInput
                     inputBoxStyle='col-md-6 offset-md-3'
+                    :inputStyle="isError.email"
                     type='email'
                     identifier='email'
                     label='Email Address'
-                    v-model="user.email"
+                    v-model.lazy.trim="user.email"
+                    @input="user.email.match(emailRegex) ? isError.email = 'is-valid' : isError.email = 'is-invalid'"
                 />
                 <formInput
                     inputBoxStyle='col-md-6 offset-md-3'
+                    :inputStyle="isError.password"
                     type='password'
                     identifier='password'
                     label='Password'
-                    v-model="user.password"
+                    v-model.lazy.trim="user.password"
+                    @input="user.password.match(passwordRegex) ? isError.password = 'is-valid' : isError.password = 'is-invalid'"
+                    invalidMsg='Password should contain an uppercase, lowercase and digit'
                 />
                 <div class="d-flex flex-column justify-content-center align-items-center">
-                    <button class="btn col-md-6 col-12 btn-login mt-3" id="submit-btn" type="submit">Sign In</button>
+                    <button class="btn col-md-6 col-12 btn-login mt-3" id="submit-btn" type="submit" :disabled='!isComplete'>Sign In</button>
                     <div class="d-flex col-md-6 col-12 justify-content-between fst-italic mt-2">
                         <p class='labelForm text--primary'> Don't have an account yet? <router-link to="/signup">Sign Up</router-link></p>
                         <router-link to="/forgot-password" class="text-decoration-none"><p class='labelForm text--primary'>Forgot Password?</p></router-link>
@@ -45,8 +50,16 @@ export default {
             user: {
                 email: '',
                 password: '',
-            }
+            },
+            isError: {},
+            emailRegex: /\b[\w-]+@[\w-]+\.\w{2,4}\b/gi,
+            passwordRegex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
         }
-     }
+     },
+    computed: {
+        isComplete () {
+            return this.user.email && this.user.password
+        }
+    }
 }
 </script>
