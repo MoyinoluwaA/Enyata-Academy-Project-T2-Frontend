7c39ee9b8f388729ac42be4bddbade3b78a151b9
Temitope3665 <aroyewontope@gmail.com>
2021-12-04 19:39:20 +0100

    filling the information for the user from the api response

--- src/views/dashboard/ApplicantDashboard.vue
+++ src/views/dashboard/ApplicantDashboard.vue
@@ -2,21 +2,33 @@
     <div class="dashboard-main-content">
         <div>
             <h2 class="dashboard-header-wrapper">Dashboard</h2>
-            <p class="fst-italic fs-6">Your Application is currently being review, you wil be notified if successful</p>
+            <p class="fst-italic fs-6" v-if="status === 'approved'">Your Application is approved, kindly watch the space to know your assessment date</p>
+            <p class="fst-italic fs-6" v-else-if="status === 'pending'">Your Application is currently being review, you wil be notified if successful</p>
+            <p class="fst-italic fs-6" v-else>Sorry, you didn't meet the requirement for this current application, kindly wait till next batch</p>
         </div>
 
         <div class="dashboard-content d-flex flex-column flex-sm-row">
             <div>
                 <p class="application-date">Date of Application</p>
-                <h3 class="text-large">09.09.19</h3>
+                <h3 class="text-large">{{ assessment_start_date.slice(0, 10) }}</h3>
                 <img src="../../assets/icons/date-of-application-status.svg" alt="status" />
-                <p class="application-sub-text">4 days since applied</p>
+                <p class="application-sub-text" v-if="status === 'approved'">
+                    <span>
+                        {{ 
+                            (Number((assessmentDate.replace('-', '').replace('-', '').slice(0, 8))) - 
+                            Number(currentDate.toISOString().replace('-', '').replace('-', '').slice(0, 8)))
+                        }}
+                    </span> days since applied</p>
+                <p class="application-sub-text" v-else>Kindly wait till next batch</p>
             </div>
             <div class="application-status mt-4 mt-sm-0">
                 <p class="application-status-text">Application Status</p>
-                <h3 class="text-large">Pending</h3>
+                <h3 class="text-large" v-if="status === 'approved'">{{ status.replace('a', 'A') }}</h3>
+                <h3 class="text-large" v-else>{{ status.replace('p', 'P') }}</h3>
                 <img src="../../assets/icons/pending-status.svg" alt="status" />
-                <p class="application-sub-text">We will get back to you</p>
+                <p class="application-sub-text" v-if="status === 'approved'">Status approved</p>
+                <p class="application-sub-text" v-else-if="status === 'pending'">We will get back to you</p>
+                <p class="application-sub-text" v-else>Kindly look forward to the next batch</p>
             </div>
         </div>
 
@@ -25,9 +37,7 @@
                 <div class="update-wrapper me-auto">
                     <p class="p-4 fw-bold">Updates</p>
                     <div class="text-center">
-                        <img class="update-lines p-4 img-fluid" src="../../assets/icons/update-line.svg" alt="line" />
-                        <img class="update-lines p-4 img-fluid" src="../../assets/icons/update-line.svg" alt="line" />
-                        <img class="update-lines p-4 img-fluid" src="../../assets/icons/update-line.svg" alt="line" />
+                        <p class="dashboard-instructions">{{ instructions }} and stop complaining</p>
                         <img class="update-lines p-4 img-fluid" src="../../assets/icons/update-line.svg" alt="line" />
                     </div>
                 </div>
@@ -36,7 +46,27 @@
                     <div class="update-wrapper mt-4 mt-lg-0 ms-auto">
                         <p class="p-4 fw-bold">Take Assessment</p>
                         <div class="assessment-cta">    
-                            <p class="fs-6 fw-normal text-center mt-auto mb-auto">We have 4 days left until the next assessment.<br/>Watch this space</p>
+                            <p class="fs-6 fw-normal text-center mt-auto mb-auto" v-if="status === 'approved'">We have 
+                                <span>
+                                    {{ 
+                                        (Number((assessmentDate.replace('-', '').replace('-', '').slice(0, 8))) - 
+                                        Number(currentDate.toISOString().replace('-', '').replace('-', '').slice(0, 8)))
+                                    }}    
+                                </span> 
+                                days left until the next assessment.<br/>Watch this space
+                            </p>
+
+                            <p class="fs-6 fw-normal text-center mt-auto mb-auto" v-else-if="status === 'pending'">We have 
+                                <span>
+                                    {{ 
+                                        (Number((assessmentDate.replace('-', '').replace('-', '').slice(0, 8))) - 
+                                        Number(currentDate.toISOString().replace('-', '').replace('-', '').slice(0, 8)))
+                                    }}    
+                                </span> 
+                                days left until the next assessment.<br/>Watch this space
+                            </p>
+
+                            <p class="fs-6 fw-normal text-center mt-auto mb-auto" v-else>No assessment for you for this batch</p>
                             <router-link :to="{ name: 'TakeAssessment'}">
                                 <Button btnText='Take Assessment' btnStyle='btn--gray mt-4' />
                             </router-link>
@@ -50,10 +80,41 @@
 
 <script>
 import Button from "../../components/Button.vue"
+import ApplicationService from '@/services/application'
 
 export default {
     components: {
         Button
+    },
+    data() {
+        return {
+            assessment_start_date: '',
+            status: '',
+            currentDate: '',
+            assessmentDate: '',
+            instructions: ''
+        }
+    },
+    async mounted() {
+        try {
+            this.currentDate = new Date()
+
+            const res = await ApplicationService.getApplicantStatus()
+            if (res.code === 200) {
+                this.assessment_start_date = res.data.academy.assessment_start_date
+                this.status = res.data.applicant.status
+                this.assessmentDate = res.data.academy.assessment_start_date
+                this.instructions = res.data.academy.academy_instruction
+            }
+        } catch (error) {
+            if (error.response.data.status === 401) {
+                this.$dtoast.pop({
+                    preset: "error",
+                    heading: "Unauthenticated user",
+                    content: "Kindly go back to sign in",
+                })
+            }
+        }
     }
 }
 </script>
