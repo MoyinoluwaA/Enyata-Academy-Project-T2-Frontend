00f31763c9531f379e9a1b152e2ba59204f88362
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-12-07 08:41:00 +0100

    removed timer on completed page

--- src/views/dashboard/AssessmentCompleted.vue
+++ src/views/dashboard/AssessmentCompleted.vue
@@ -5,19 +5,9 @@
                 <h2 class="dashboard-header">Take Assessment</h2>
                 <p class="fst-italic fs-6">Thank you!</p>
             </div>
-
-            <div class="assessment-timer-wrapper mt-4 mt-md-0">
-                <p class="assessment-timer-text">Timer</p>
-                <p class="timer">
-                    <span class="timer">23</span>
-                    <span class="timer-min">min</span>
-                    <span>043</span>
-                    <span class="timer-sec">sec</span>  
-                </p>
-            </div>
         </div>
 
-        <div class="assessment-content mb-5">
+        <div class="assessment-content px-5 mb-5">
             <img class="load-icon" src="../../assets/icons/congratulatory-img.svg" alt="congratulations" />
             <p class="ms-auto me-auto text-center mt-4">We have received your assessment test, we will get back to you soon.<br/>Best of luck</p>
             
