5081b6e7ad1864a37bc345454cb21ed15c1894d7
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-12-10 09:02:33 +0100

    finalized sidebar

--- src/components/ApplicantSideBar.vue
+++ src/components/ApplicantSideBar.vue
@@ -2,46 +2,45 @@
         <div>
             <div class="d-md-none nav-bar">
                 <nav class="navbar navbar-light bg-light">
-                    <div class="container-fluid">
+                    <div class="container-fluid p-0">
+                        <div class="px-2 d-flex justify-content-between align-items-center w-100">
                             <router-link class="navbar-brand" :to="{ name: 'ApplicantDashboard'}">
                                 <img src="../assets/icons/enyata-logo.svg" />
                             </router-link>
-                        <div class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <div class="open-slide">
-                                <img class="hamburger" src="../assets/icons/hamburger-colored.png" alt="hamburger" />
+                            <div class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <div class="open-slide">
+                                    <img class="hamburger" src="../assets/icons/hamburger-colored.png" alt="hamburger" />
+                                </div>
                             </div>
                         </div>
 
                         <div class="d-md-none collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-                                <div class="side-bar-top">
-                                    <img class="profile-img" :src="image" alt="profile-img">
-
-                                    <p class="applicant-name text-center"><span>{{ first_name }}</span> <span class="ms-1">{{ last_name }}</span></p>
+                            <div class="side-bar-top">
+                                <img class="profile-img" :src="image" alt="profile-img">
+                                <p class="applicant-name text-center">{{ first_name }} 
+                                    <span class="ms-1">{{ last_name }}</span>
+                                </p>
                                 <p class="applicant-mail text-center">{{ email }}</p>
-                                </div>
-                                <li class="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                                    <div class="side-bar-info">
-                                        <div class="dashboard-menus">
-                                            <router-link class="dashboard-link" :to="{ name: 'ApplicantDashboard'}">
-                                                <img class="dashboard-img" src="../assets/icons/dashboard-icon.svg" alt="dashboard"/>
-                                                <span class="dashboad-menu">Dashboard</span>
-                                            </router-link>
-                                        </div>
+                            </div>
 
-                                        <div class="assessment-menus">
-                                            <router-link class="dashboard-link" :to="{ name: 'TakeAssessment' }">
-                                                <img class="assessment-img" src="../assets/icons/assessment-icon.svg" alt="assement"/>
-                                                <span class="assessment-menu">Assessment</span>
-                                            </router-link>
-                                        </div>
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 side-bar-info">
+                                <li class="dashboard-menus" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                                    <router-link class="dashboard-link" :to="{ name: 'ApplicantDashboard'}">
+                                        <img class="dashboard-img" src="../assets/icons/dashboard-icon.svg" alt="dashboard"/>
+                                        <span class="dashboad-menu">Dashboard</span>
+                                    </router-link>
+                                </li>
 
-                                        <div class="logout-menus ms-3" @click="logOut">
-                                            <img class="logout-img" src="../assets/icons/logout-icon.svg" alt="logout"/>
-                                            <span class="logout-menu">Log Out</span>
-                                        </div>
-                                    </div>
+                                <li class="assessment-menus" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                                    <router-link class="dashboard-link" :to="{ name: 'TakeAssessment' }">
+                                        <img class="assessment-img" src="../assets/icons/assessment-icon.svg" alt="assement"/>
+                                        <span class="assessment-menu">Assessment</span>
+                                    </router-link>
+                                </li>
+
+                                <li class="logout-menus ms-3" @click="logOut">
+                                    <img class="logout-img" src="../assets/icons/logout-icon.svg" alt="logout"/>
+                                    <span class="logout-menu">Log Out</span>
                                 </li>
                             </ul>
                         </div>
@@ -51,22 +50,23 @@
             <div class="side-bar d-none d-md-block">
                 <div class="side-bar-top">
                     <img class="profile-img" :src="image" alt="profile-img">
-
-                    <p class="applicant-name text-center"><span>{{ first_name }}</span> <span class="ms-1">{{ last_name }}</span></p>
+                    <p class="applicant-name text-center"> {{ first_name }}
+                        <span class="ms-1">{{ last_name }}</span>
+                    </p>
                     <p class="applicant-mail text-center">{{ email }}</p>
                 </div>
                 
                 <div class="side-bar-info">
-                        <div class="dashboard-menus">
-                            <router-link class="dashboard-link" :to="{ name: 'ApplicantDashboard'}">
-                                <img class="dashboard-img" src="../assets/icons/dashboard-icon.svg" alt="dashboard"/>
-                                <span class="dashboad-menu">Dashboard</span>
-                            </router-link>
-                        </div>
+                    <div class="dashboard-menus">
+                        <router-link class="dashboard-link" :to="{ name: 'ApplicantDashboard'}">
+                            <img class="dashboard-img" src="../assets/icons/dashboard-icon.svg" alt="dashboard"/>
+                            <span class="dashboad-menu">Dashboard</span>
+                        </router-link>
+                    </div>
 
                     <div class="assessment-menus">
                         <router-link class="dashboard-link" :to="{ name: 'TakeAssessment' }">
-                            <img class="assessment-img" src="../assets/icons/assessment-icon.svg" alt="assement"/>
+                            <img class="assessment-img" src="../assets/icons/assessment-icon.svg" alt="assessment"/>
                             <span class="dashboard-menu">Assessment</span>
                         </router-link>
                     </div>
